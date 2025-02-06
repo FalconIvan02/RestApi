eeafb8786420ab0e2b0ca36df93af4b217a01714
Ivansito <falconivanernesto@gmail.com>
2025-02-06 11:53:14 -0300

    small change

--- index.js
+++ index.js
@@ -1,40 +1,93 @@
 const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
 const app = express()
-app.disable('x-powered-by')
+const cors = require('cors')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 app.use(express.json())
-// app.use((req, res, next) => {
-//     if (req.method !== 'POST') return next()
-//     if (req.headers['content-type'] !== 'application/json') return next()
-
-//     // Aca solo llegan request que son POST y que tienen el header content type application json
-//     let body = ''
-
-//     req.on('data', (chunk) => {
-//         body += chunk.toString()
-//     })
-
-//     req.on('end', () => {
-//         const data = JSON.parse(body)
-//         data.timestamp = Date.now()
-//         req.body = data
-//         next()
-//     })
-// })
-
-const PORT = process.env.PORT ?? 3000
+app.use(cors())
+app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
-    res.send('<h1>Bienvenido!</h1>')
+    res.json({ message: 'Welcome to my page' })
+})
+// Todos los recursos que sean MOVIES se identifica con /movies
+app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter((movie) =>
+            movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
 })
 
-app.post('/leon', (req, res) => {
-    // req.body deberiamos guardar en la base de datos
-    res.status(201).json(req.body)
+// Recuperar peliculas por ID
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find((movie) => movie.id === id)
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
 })
 
-app.use('*', (req, res) => {
-    res.status(404).send('<h1>404 not found</h1>')
+// Crear peliculas
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessable Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+
+    //Esto no seria Rest, porque estamos guardando el estado en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
 })
 
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
+// Eliminar peliculas
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (!movieIndex < 0) return res.status(404).json({ message: 'Movie not found' })
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted' })
+})
+
+// Actualizar peliculas
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessable Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (!movieIndex < 0) return res.status(404).json({ message: 'Movie not found' })
+
+    const updatedMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updatedMovie
+
+    return res.json(updatedMovie)
+})
+const PORT = process.env.PORT ?? 3000
+module.exports = app
+app.listen(PORT, () => {
+    console.log(`Listening on port http://localhost:${PORT}`)
+})
